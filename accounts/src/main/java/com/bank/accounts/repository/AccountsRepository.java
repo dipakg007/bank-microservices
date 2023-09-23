2f57917b79dabf865a8c8cf20d30500f17b6ac11
Dipak Gupta <dipakg286@gmail.com>
2023-09-23 22:15:46 +0530

    Documentation of account service and validation done

--- accounts/src/main/java/com/bank/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/bank/accounts/repository/AccountsRepository.java
@@ -1,12 +1,20 @@
 package com.bank.accounts.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bank.accounts.entity.Accounts;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
-    Accounts findByCustomerId(int customerId);
+    Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Modifying
+    @Transactional
+    void deleteByCustomerId(Long customerId);
 }
