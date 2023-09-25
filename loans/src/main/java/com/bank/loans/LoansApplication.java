57d7dff74a649b1b0ceda778e0352ac5039808fa
Dipak Gupta <dipakg286@gmail.com>
2023-09-25 20:19:06 +0530

    Loans service changes with exception handling and new service

--- loans/src/main/java/com/bank/loans/LoansApplication.java
+++ loans/src/main/java/com/bank/loans/LoansApplication.java
@@ -1,16 +1,40 @@
 package com.bank.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScans({@ComponentScan("com.bank.loans.controller")})
-@EnableJpaRepositories("com.bank.loans.repository")
-@EntityScan("com.bank.loans.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "Bank Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Dipak Gupta",
+                        email = "dipakg286@gmail.com",
+                        url = "https://www.linkedin.com/in/dipak-gupta-75b15018b/"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.linkedin.com/in/dipak-gupta-75b15018b/"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Bank Loans microservice REST API Documentation",
+                url = "https://www.linkedin.com/in/dipak-gupta-75b15018b/"
+        )
+)
 public class LoansApplication {
 
     public static void main(String[] args) {
--- loans/src/main/java/com/bank/loans/repository/LoansRepository.java
+++ loans/src/main/java/com/bank/loans/repository/LoansRepository.java
@@ -1,13 +1,17 @@
 package com.bank.loans.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bank.loans.model.Loans;
+import com.bank.loans.entity.Loans;
 
 @Repository
-public interface LoansRepository extends CrudRepository<Loans, Long> {
-    List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
 }
